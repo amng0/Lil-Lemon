788aab3cb8546c3b3f292ef4c97ced09e1a74500
Amie "Lotta Hoes" Nguyen <a.nguyen05@icloud.com>
2024-11-26 15:54:47 -0700

    Initial commit

---
+++ LittleLemonApp/LittleLemonApp/Views/Home.swift
@@ -0,0 +1,22 @@
+//
+//  Home.swift
+//  LittleLemonApp
+//
+//  Created by Amie Nguyen on Nov 24th, 2024
+//
+
+import SwiftUI
+
+struct Home: View {
+    
+    var body: some View {
+        MainScreen()
+            .navigationBarBackButtonHidden()
+    }
+}
+
+struct Home_Previews: PreviewProvider {
+    static var previews: some View {
+        Home().environment(\.managedObjectContext, PersistenceController.shared.container.viewContext)
+    }
+}
---
+++ littlemon/littlemonTests/littlemonTests.swift
@@ -0,0 +1,17 @@
+//
+//  littlemonTests.swift
+//  littlemonTests
+//
+//  Created by Amie Nguyen on 11/26/24.
+//
+
+import Testing
+@testable import littlemon
+
+struct littlemonTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
